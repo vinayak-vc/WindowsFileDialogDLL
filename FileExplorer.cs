d3a43c3177fd9e405b2b66ab465ace489eab58f8
Vinayak Lakhani <47971927+vinayak-vc@users.noreply.github.com>
2025-09-26 12:10:02 +0530

    Update FileExplorer.cs

--- FileExplorer.cs
+++ FileExplorer.cs
@@ -1,29 +1,30 @@
 using System;
 using System.Runtime.InteropServices;
 
-public static class FileExplorer
-{
-    [DllImport("WindowsFileDialog", CharSet = CharSet.Unicode)]
-    private static extern IntPtr OpenWindowsFile();
+public static class FileExplorer {
+    private const string WindowsFileDialog = "WindowsFileDialog.dll";
 
-    [DllImport("WindowsFileDialog", CharSet = CharSet.Unicode)]
-    private static extern IntPtr OpenFileWithExtension(string extensions);
-    
-    [DllImport("WindowsFileDialog", CharSet = CharSet.Unicode)]
-    private static extern IntPtr OpenFolderDialog(string extensions);
+    [DllImport(WindowsFileDialog, CharSet = CharSet.Unicode)]
+    private static extern IntPtr OpenWindowsFile(string initialDir);
 
-    public static string OpenFileExplorer() {
-        IntPtr ptr = OpenWindowsFile();
+    [DllImport(WindowsFileDialog, CharSet = CharSet.Unicode)]
+    private static extern IntPtr OpenFileWithExtension(string filter, string initialDir);
+
+    [DllImport(WindowsFileDialog, CharSet = CharSet.Unicode)]
+    private static extern IntPtr OpenFolderDialog(string initialDir);
+
+    public static string OpenFileExplorer(string initialDir = null) {
+        IntPtr ptr = OpenWindowsFile(initialDir);
         return Marshal.PtrToStringUni(ptr);
     }
 
-    public static string OpenFileExplorer(string filter) {
-        IntPtr ptr = OpenFileWithExtension(filter);
+    public static string OpenFileExplorer(string filter, string initialDir = null) {
+        IntPtr ptr = OpenFileWithExtension(filter, initialDir);
         return Marshal.PtrToStringUni(ptr);
     }
-    
-    public static string OpenFolder(string filter) {
-        IntPtr ptr = OpenFolderDialog(filter);
+
+    public static string OpenFolder(string initialDir = null) {
+        IntPtr ptr = OpenFolderDialog(initialDir);
         return Marshal.PtrToStringUni(ptr);
     }
 }
